3a59f3e1646532b58ce6b6ceb1496cf7983a056f
Rojan <learncode.rojan@gmail.com>
2022-04-09 12:17:45 +1000

    Completed the modal of the site
    Signed-off-by: Rojan <learncode.rojan@gmail.com>

---
+++ index.js
@@ -0,0 +1,66 @@
+let contrastToggle = false;
+let isModalOpen = false;
+let scaleFactorMoveSmallIcons =1/20;
+
+
+//To send message to us
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible" ;
+
+    emailjs.sendForm(
+        'service_638l3p6',
+        'template_2t5063a',
+        event.target,
+        'KSmalh_y7_ETD6Aqn'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += "  modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert("Email Sevice is temporarily unavailable. Please contact me directly: rojan108p@gmail.com");
+    })
+}
+
+//for opening and closing Modal(Contact PopUp)
+
+function toggleMenu(){
+    if(isModalOpen){
+            isModalOpen =false;
+           return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = !isModalOpen;
+    document.body.classList += " modal--open";
+}
+
+// Dark/Light Mode Toggle
+
+function toggleContrast(){
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+      document.body.classList += " dark-theme"
+    }
+    else {
+      document.body.classList.remove("dark-theme")
+    }
+}
+
+//to move icons as we move mouse
+function moveSmallIcons(event){
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactorMoveSmallIcons;
+    const y= event.clientY * scaleFactorMoveSmallIcons;
+    for(let i = 0; i<shapes.length; ++i){
+        const numODD=i%2 !==0;
+        const boolINT = numODD? -1:1;
+        shapes[i].style.transform = `translate(${x*boolINT}px, ${y*boolINT}px) rotate(${x}deg) `;
+    }
+
+}
+
+
+
+
+
